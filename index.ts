2a9c3cd28100d598ae7b86f2db979c2c3f1aac36
Ozan Erturk <ozan@relive.cc>
2022-12-17 14:29:27 +0100

    basic setup

---
+++ index.ts
@@ -0,0 +1,11 @@
+export {
+  Op,
+  QueryTypes,
+  fn,
+  col,
+  literal,
+  where,
+  UniqueConstraintError,
+  WhereOptions,
+  Transaction,
+} from "sequelize";
---
+++ src/models/movie.ts
@@ -0,0 +1,35 @@
+/**
+ *
+ * @export
+ * @interface moviePjo
+ */
+
+export interface moviePojo {
+  /**
+   * Primary key
+   *
+   * @type {number}
+   */
+  id: number;
+
+  /**
+   * Title
+   *
+   * @type {string}
+   */
+  title: string;
+
+  /**
+   * Description
+   *
+   * @type {string}
+   */
+  description: string;
+
+  /**
+   * Year of publish
+   *
+   * @type {number}
+   */
+  year: number;
+}
